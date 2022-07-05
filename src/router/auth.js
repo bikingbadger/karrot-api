2e58ecbcd79a2406114a21165c3b95479cff6dae
Hilton Meyer <development@hiltonmeyer.com>
2022-07-05 17:28:30 +0300

    [feat](user): check that user does not exist

--- src/router/auth.js
+++ src/router/auth.js
@@ -6,13 +6,21 @@ const authRouter = Router();
 authRouter.post('/register', async (req, res) => {
   // Validate data
   const error = await registerValidation(req.body);
-  console.log(error);
   if (error) return res.status(error.status).json(error);
+
+  // Create user object
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
   });
+
+  // Check user doesn't exist in DB
+  const exists = await User.findOne({ email: req.body.email });
+  if (exists)
+    return res.status(400).json({ error: { message: 'User exists' } });
+
+  // Save the user
   try {
     const savedUser = await user.save();
     res.status(200).json(savedUser);
