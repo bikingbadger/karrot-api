e5e305ef720687e8e8b3147efe6464f5950893e5
Hilton Meyer <development@hiltonmeyer.com>
2022-07-05 18:15:32 +0300

    [feat](user): add jwt

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import User from '../model/User.js';
 import { registerValidation, loginValidation } from '../utils/validation.js';
-import { hashValue } from '../utils/encryption.js';
+import { hashValue, compareValues, jwtToken } from '../utils/encryption.js';
 
 const authRouter = Router();
 
@@ -25,10 +25,30 @@ authRouter.post('/register', async (req, res) => {
   // Save the user
   try {
     const savedUser = await user.save();
-    res.status(200).json({id: savedUser._id});
+    res.status(200).json({ id: savedUser._id });
   } catch (error) {
     res.status(400).send(error);
   }
+});
+
+authRouter.post('/login', async (req, res) => {
+  // Validate data
+  const error = await loginValidation(req.body);
+  if (error) return res.status(error.status).json(error);
+
+  // Check if email exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user)
+    return res.status(400).json({ error: { message: 'User does not exist' } });
+
+  // Check passwords match
+  const passwordsMatch = await compareValues(req.body.password, user.password);
+  console.log(req.body.password, user.password, passwordsMatch);
+  if (!passwordsMatch)
+    return res.status(400).json({ error: { message: 'User does not exist' } });
+  res
+    .status(200)
+    .json({ status: 'success', token: jwtToken({ id: user._id }) });
 });
 
 export default authRouter;
--- src/utils/encryption.js
+++ src/utils/encryption.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const hashValue = async (inputValue) => {
   const salt = await bcrypt.genSalt(10);
@@ -6,4 +7,12 @@ const hashValue = async (inputValue) => {
   return hashValue;
 };
 
-export { hashValue };
+const compareValues = async (source, target) => {
+  return await bcrypt.compare(source, target);
+};
+
+const jwtToken = (inputValue) => {
+  return jwt.sign(inputValue, process.env.JWT_TOKEN);
+};
+
+export { hashValue, compareValues, jwtToken };
