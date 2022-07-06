0538133438e7a89402a320e2230530cd5bb46b10
Hilton Meyer <development@hiltonmeyer.com>
2022-07-06 11:07:09 +0300

    [fix](setup): debug db connection

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,21 +1,18 @@
 import mongoose from 'mongoose';
 
+console.log(process.env);
 // The URL should be in the .env file locally or an environment
 // variable when hosted in the cloud
 const uri = process.env.DB_CONNECT;
-
-console.log(process.env);
+console.log('uri', uri);
 
 const dbConnection = mongoose
-  .connect(
-    uri,
-    { dbName : "Karrot" },
-  )
+  .connect(uri, { dbName: 'Karrot' })
   .then(() => {
     console.log('connected to db');
   })
   .catch((err) => {
-    console.log('DB Connection Error: ',err);
+    console.log('DB Connection Error: ', err);
   });
 
 export { dbConnection };
