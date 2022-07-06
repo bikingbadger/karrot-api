8b208d05c87d150eeea0084488c677dd0cd190e0
Hilton Meyer <development@hiltonmeyer.com>
2022-07-06 11:03:45 +0300

    [feat](setup): add welcome message for api

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 const router = Router();
 import authRouter from './auth.js';
-import kidRouter from './kid.js'
-import {validToken} from '../utils/auth.js'
+import kidRouter from './kid.js';
+import { validToken } from '../utils/auth.js';
+
+router.get('/', (req, res) => {
+  res.status(200).send('Welcome to Karrot');
+});
 
 router.use('/user', authRouter);
-router.use('/kid',validToken, kidRouter);
+router.use('/kid', validToken, kidRouter);
 
 export default router;
